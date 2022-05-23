87c2545e0d9b58551852e12b1dbcc0ff9a0492c3
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-23 08:57:17 +0100

    Update SpaceInvadersGame.java

--- SpaceInvadersGame.java
+++ SpaceInvadersGame.java
@@ -1,10 +1,15 @@
 package com.codegym.games.spaceinvaders;
 
 import com.codegym.engine.cell.*;
+import com.codegym.games.spaceinvaders.gameobjects.Star;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceInvadersGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private List<Star> stars;
 
     @Override
     public void initialize() {
@@ -12,22 +17,32 @@ public class SpaceInvadersGame extends Game {
         createGame();
     }
 
-    private void createGame(){
+    private void createGame() {
+        createStars();
         drawScene();
     }
 
-    private void drawScene(){
+    private void drawScene() {
         drawField();
     }
 
-    private void drawField(){
+    private void drawField() {
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 setCellValueEx(i, j, Color.BLACK, "");
             }
         }
+
+        for (Star star : stars) {
+            star.draw(this);
+        }
+    }
+
+    private void createStars() {
+        stars = new ArrayList<Star>();
+        for (int i = 0; i < 8; i++) {
+            stars.add(new Star(i, i));
+        }
     }
 }
-
-
 
