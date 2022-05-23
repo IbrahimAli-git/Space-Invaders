77ec5ab60fe22231e16c6d10a4d1f758ad74998c
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-23 09:45:07 +0100

    Update Star.java

--- gameobjects/Star.java
+++ gameobjects/Star.java
@@ -1,17 +1,15 @@
 package com.codegym.games.spaceinvaders.gameobjects;
 
-import com.codegym.engine.cell.Color;
 import com.codegym.engine.cell.*;
-import com.codegym.games.spaceinvaders.gameobjects.GameObject;
 
 public class Star extends GameObject {
-    private static final String STAR_SIGN = "\\u2605";
+    private static final String STAR_SIGN = "\u2605";
 
     public Star(double x, double y) {
         super(x, y);
     }
 
-    public void draw(Game game){
+    public void draw(Game game) {
         game.setCellValueEx((int) x, (int) y, Color.NONE, STAR_SIGN, Color.WHITE, 100);
     }
 }
