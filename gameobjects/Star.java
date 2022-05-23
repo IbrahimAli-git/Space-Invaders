7101455d42712f94b2a6ccb5a59d655c03b496cf
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-23 08:44:36 +0100

    Add files via upload

---
+++ gameobjects/Star.java
@@ -0,0 +1,9 @@
+package com.codegym.games.spaceinvaders.gameobjects;
+
+import com.codegym.games.spaceinvaders.gameobjects.GameObject;
+
+public class Star extends GameObject {
+    public Star(double x, double y) {
+        super(x, y);
+    }
+}
