7c7da959287ecef39dc1682c6bc15ba85c834439
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-24 07:54:04 +0100

    Update Ship.java

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -1,5 +1,7 @@
 package com.codegym.games.spaceinvaders.gameobjects;
 
+import com.codegym.games.spaceinvaders.Direction;
+
 public class Ship extends GameObject {
     public Ship(double x, double y) {
         super(x, y);
@@ -7,5 +9,9 @@ public class Ship extends GameObject {
 
     public void setStaticView(int[][] viewFrame){
         setMatrix(viewFrame);
+    }
+
+    public Bullet fire(){
+        return null;
     }
 }
