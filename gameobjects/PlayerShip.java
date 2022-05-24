558b0c46d88ac9a5ed0f3c27f459365d1f94786a
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-24 08:26:13 +0100

    Add files via upload

---
+++ gameobjects/PlayerShip.java
@@ -0,0 +1,11 @@
+package com.codegym.games.spaceinvaders.gameobjects;
+
+import com.codegym.games.spaceinvaders.ShapeMatrix;
+import com.codegym.games.spaceinvaders.SpaceInvadersGame;
+
+public class PlayerShip extends Ship {
+    public PlayerShip(){
+        super(SpaceInvadersGame.WIDTH/2.0, SpaceInvadersGame.HEIGHT- ShapeMatrix.PLAYER.length-1);
+        setStaticView(ShapeMatrix.PLAYER);
+    }
+}
