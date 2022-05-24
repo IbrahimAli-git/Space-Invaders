493e5a106f57002ede360f4080137b4bc745e82f
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-24 09:34:27 +0100

    Update Ship.java

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -1,9 +1,13 @@
 package com.codegym.games.spaceinvaders.gameobjects;
 
-import com.codegym.games.spaceinvaders.Direction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Ship extends GameObject {
     public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
         super(x, y);
@@ -11,6 +15,9 @@ public class Ship extends GameObject {
 
     public void setStaticView(int[][] viewFrame){
         setMatrix(viewFrame);
+        frames = new ArrayList<>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public Bullet fire(){
@@ -20,4 +27,11 @@ public class Ship extends GameObject {
     public void kill(){
         isAlive = false;
     }
+
+    public void setAnimatedView(int[][]... viewFrames){
+        setMatrix(viewFrames[0]);
+        frames = Arrays.asList(viewFrames);
+        frameIndex = 0;
+    }
 }
+
