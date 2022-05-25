9d19b633c72500eae144b973a92d8c7f97bff28f
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-25 11:14:22 +0100

    Update PlayerShip.java

--- gameobjects/PlayerShip.java
+++ gameobjects/PlayerShip.java
@@ -3,56 +3,34 @@ package com.codegym.games.spaceinvaders.gameobjects;
 import com.codegym.games.spaceinvaders.Direction;
 import com.codegym.games.spaceinvaders.ShapeMatrix;
 import com.codegym.games.spaceinvaders.SpaceInvadersGame;
-
 import java.util.List;
 
 public class PlayerShip extends Ship {
     private Direction direction = Direction.UP;
 
-    public PlayerShip(){
-        super(SpaceInvadersGame.WIDTH/2.0, SpaceInvadersGame.HEIGHT- ShapeMatrix.PLAYER.length-1);
+    public PlayerShip() {
+        super(SpaceInvadersGame.WIDTH / 2., SpaceInvadersGame.HEIGHT - ShapeMatrix.PLAYER.length - 1);
         setStaticView(ShapeMatrix.PLAYER);
+    }
+
+    public void setDirection(Direction newDirection) {
+        if (newDirection != Direction.DOWN) {
+            this.direction = newDirection;
+        }
     }
 
     public Direction getDirection() {
         return direction;
     }
 
-    public void setDirection(Direction newDirection) {
-        if (newDirection != Direction.DOWN){
-            this.direction = newDirection;
-        }
-    }
-
-    public void move(){
-        if (!isAlive) return;
-        switch (direction){
-            case LEFT: x--; break;
-            case RIGHT: x++;
-        }
-        if (x < 0){
-            x = 0;
-        } if (x + width > SpaceInvadersGame.WIDTH){
-            x = SpaceInvadersGame.WIDTH - width;
-        }
-    }
-
-    @Override
-    public Bullet fire() {
-        if (!isAlive) return null;
-        return new Bullet(x + 2, y - ShapeMatrix.BULLET.length, Direction.UP);
-    }
-
-    @Override
-    public void kill() {
-        if (!isAlive) return;
-        isAlive = false;
-        super.setAnimatedView(ShapeMatrix.KILL_PLAYER_ANIMATION_FIRST, ShapeMatrix.KILL_PLAYER_ANIMATION_SECOND, ShapeMatrix.KILL_PLAYER_ANIMATION_THIRD, ShapeMatrix.DEAD_PLAYER);
+    public void win() {
+        setStaticView(ShapeMatrix.WIN_PLAYER);
     }
 
     public void checkHit(List<Bullet> bullets) {
-        if (bullets.isEmpty()) return;
-
+        if (bullets.isEmpty()) {
+            return;
+        }
         if (isAlive) {
             for (Bullet bullet : bullets) {
                 if (isAlive && bullet.isAlive && isCollision(bullet)) {
@@ -61,6 +39,44 @@ public class PlayerShip extends Ship {
                     return;
                 }
             }
+        }
+    }
+
+    @Override
+    public void kill() {
+        if (!isAlive) {
+            return;
+        }
+        isAlive = false;
+        super.setAnimatedView(false, ShapeMatrix.KILL_PLAYER_ANIMATION_FIRST, ShapeMatrix.KILL_PLAYER_ANIMATION_SECOND, ShapeMatrix.KILL_PLAYER_ANIMATION_THIRD, ShapeMatrix.DEAD_PLAYER);
+    }
+
+    @Override
+    public Bullet fire() {
+        if (!isAlive) {
+            return null;
+        }
+        return new Bullet(x + 2, y - ShapeMatrix.BULLET.length, Direction.UP);
+    }
+
+
+    public void move() {
+        if (!isAlive) {
+            return;
+        }
+
+        if (direction == Direction.LEFT) {
+            x--;
+        }
+        if (direction == Direction.RIGHT) {
+            x++;
+        }
+
+        if (x < 0) {
+            x = 0;
+        }
+        if (x + width > SpaceInvadersGame.WIDTH) {
+            x = SpaceInvadersGame.WIDTH - width;
         }
     }
 }
