723576ad67250770444c432223c1e0caf22c14a3
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-25 11:15:13 +0100

    Update Ship.java

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -2,48 +2,55 @@ package com.codegym.games.spaceinvaders.gameobjects;
 
 import com.codegym.engine.cell.Game;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Ship extends GameObject {
     public boolean isAlive = true;
     private List<int[][]> frames;
     private int frameIndex;
+    private boolean loopAnimation = false;
 
     public Ship(double x, double y) {
         super(x, y);
     }
 
-    public void setStaticView(int[][] viewFrame){
+    public Bullet fire() {
+        return null;
+    }
+
+    public void setStaticView(int[][] viewFrame) {
         setMatrix(viewFrame);
         frames = new ArrayList<>();
         frames.add(viewFrame);
         frameIndex = 0;
     }
 
-    public Bullet fire(){
-        return null;
-    }
-
-    public void kill(){
+    public void kill() {
         isAlive = false;
     }
 
-    public void setAnimatedView(int[][]... viewFrames){
-        setMatrix(viewFrames[0]);
-        frames = Arrays.asList(viewFrames);
-        frameIndex = 0;
+    public void setAnimatedView(boolean isLoopAnimation, int[][]... viewFrames){
+        loopAnimation = isLoopAnimation;
     }
 
-    public void nextFrame(){
-        if (frameIndex >= frames.size()-1) return;
+
+    public void nextFrame() {
         frameIndex++;
+        if (frameIndex >= frames.size() && !loopAnimation) {
+            return;
+        } else if (frameIndex >= frames.size()){
+            frameIndex = 0;
+        }
         matrix = frames.get(frameIndex);
     }
 
-    @Override
+
     public void draw(Game game) {
         super.draw(game);
         nextFrame();
+    }
+
+    public boolean isVisible() {
+        return isAlive || frameIndex < frames.size();
     }
 }
