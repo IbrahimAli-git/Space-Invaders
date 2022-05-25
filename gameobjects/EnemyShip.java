d640ed797947914ff8bf8ee31f7b7176456ea939
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-05-25 11:12:59 +0100

    Update EnemyShip.java

--- gameobjects/EnemyShip.java
+++ gameobjects/EnemyShip.java
@@ -3,21 +3,46 @@ package com.codegym.games.spaceinvaders.gameobjects;
 import com.codegym.games.spaceinvaders.Direction;
 import com.codegym.games.spaceinvaders.ShapeMatrix;
 
+
 public class EnemyShip extends Ship {
+    public int score = 15;
+
     public EnemyShip(double x, double y) {
         super(x, y);
         setStaticView(ShapeMatrix.ENEMY);
     }
 
-    public void move(Direction direction, double speed){
-        switch (direction){
-            case RIGHT: x += speed; break;
-            case LEFT: x -= speed; break;
-            case DOWN: y += 2; break;
+    public void move(Direction direction, double speed) {
+        switch (direction) {
+            case RIGHT:
+                x = x + speed;
+                break;
+            case LEFT:
+                x = x - speed;
+                break;
+            case DOWN:
+                y += 2;
+                break;
+            default:
+                break;
         }
     }
 
-    public Bullet fire(){
-        return new Bullet(x+1, y+height, Direction.DOWN);
+
+    @Override
+    public Bullet fire() {
+        return new Bullet(x + 1, y + height, Direction.DOWN);
+    }
+
+    @Override
+    public void kill() {
+        if (!isAlive) {
+            return;
+        }
+        isAlive = false;
+
+        super.setAnimatedView(false, ShapeMatrix.KILL_ENEMY_ANIMATION_FIRST,
+                ShapeMatrix.KILL_ENEMY_ANIMATION_SECOND,
+                ShapeMatrix.KILL_ENEMY_ANIMATION_THIRD);
     }
 }
